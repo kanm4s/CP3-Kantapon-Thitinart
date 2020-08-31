8af4cc007c290c423be56d6af5688357d6b07f48
Kantar <kanm4s@gmail.com>
2020-08-31 15:36:37 +0700

    Add show score function
    Add main function

---
+++ Exercise4_Kantapon_T.py
@@ -0,0 +1,18 @@
+uniName = "xxx"
+print("The academic result of sophomore first semester of",uniName, "Univercity.")
+print("=================================================================")
+print("")
+
+colName = "Kantapon T."
+fdEngishScore = 80
+grBusinessScore = 70
+introComSystem = 90
+comProgramming = 100
+
+print("Name:",colName)
+print("--- Your Score ---")
+print("")
+print("Foundation English:", fdEngishScore)
+print("General Business  :", grBusinessScore)
+print("Introduction to Computer Systems:", introComSystem)
+print("Computer Programming:", comProgramming)
