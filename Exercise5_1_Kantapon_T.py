0f706e42295327e0bfcefd16d56e84b145ec3a4d
Kantar <kanm4s@gmail.com>
2020-09-02 16:04:51 +0700

    exercise calculator

---
+++ Exercise5_1_Kantapon_T.py
@@ -0,0 +1,7 @@
+firstNo = int(input("First number: "))
+secondNo = int(input("Second number: "))
+
+print(firstNo,"+",secondNo,"=",firstNo+secondNo)
+print(firstNo,"-",secondNo,"=",firstNo-secondNo)
+print(firstNo,"*",secondNo,"=",firstNo*secondNo)
+print(firstNo,"/",secondNo,"=",firstNo/secondNo)
---
+++ Exercise5_2_Kantapon_T.py
@@ -0,0 +1,4 @@
+distanceTravel = int(input("Distance (KM):"))
+timeOfTravel = int(input("Time(HR):"))
+result = int(distanceTravel/timeOfTravel)
+print(result,"km/h")
