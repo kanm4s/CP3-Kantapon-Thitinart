980e9efccbf7ce34f9f68938de758a714f548e74
Kantar <kanm4s@gmail.com>
2020-09-08 23:45:00 +0700

    Create Lecture105

---
+++ Lecture105_Kantapon_T.py
@@ -0,0 +1,26 @@
+class Vehicle:
+    licenseCode = ""
+    serialCode = ""
+    def turnOnAirConditioner(self):
+        print("Turn on: Air")
+
+class Car(Vehicle):
+    pass
+
+class Pickup(Vehicle):
+    pass
+
+class Van(Vehicle):
+    pass
+
+class EstateCar(Vehicle):
+    pass
+
+car1 = Car()
+car1.turnOnAirConditioner()
+pickup1 = Pickup()
+pickup1.turnOnAirConditioner()
+van1 = Van()
+van1.turnOnAirConditioner()
+estateCar1 = EstateCar()
+estateCar1.turnOnAirConditioner()
